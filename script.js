0d4a3c5b3915db2f16bc63534b9aa80d356d404b
example <alisayar0107@gmail.com>
2021-04-14 12:58:00 +0300

    canvas created according to the window dimensions

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const { body } = document;
 // Global Variables
 const canvas = document.createElement('canvas');
 canvas.id = 'canvas';
-const contexxt = canvas.getContext('2d');
+const context = canvas.getContext('2d');
 
 let currentSize = 10;
 let bucketColor = '#FFFFFF';
@@ -66,10 +66,10 @@ let currentColor = '#A51DAB';
 
 // Create Canvas
 function createCanvas() {
-  // canvas.width = ;
-  // canvas.height = ;
-  // context.fillStyle = ;
-  // context.fillRect();
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight - 50;
+  context.fillStyle = bucketColor;
+  context.fillRect(0, 0, canvas.width, canvas.height);
   body.appendChild(canvas);
 }
 
